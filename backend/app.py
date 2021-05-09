dac8c1b5af7f4776a9b04ff751f1cff9fc2fa494
Lasse Vad <lasse@lifekeys.no>
2021-05-09 21:01:41 +0100

    select parallelization method implemented

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,12 @@
 import subprocess
 from flask import Flask, request, send_file
 from flask_cors import CORS, cross_origin
+from flask import jsonify
+import os
+import json
+
+with open('data.json') as f:
+    binaries = json.load(f)
 
 UPLOAD_FOLDER = '/uploads'
 ALLOWED_EXTENSIONS = set(['png'])
@@ -12,9 +18,19 @@ cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
+@app.route('/uploadBinary', methods=['POST'])
+@cross_origin()
+def upload_binary():
+    global binary
+    print(request.json["data"]["parallel"])
+    binary = request.json["data"]["parallel"]
+    return "Binary recieved"
+
+
 def process_image(image_id):
-    subprocess.run(["./main", "uploads/" + image_id + ".png",
+    subprocess.run([binary, "uploads/" + image_id + ".png",
                    "uploads/" + image_id + ".png"], capture_output=False)
+    print("Processing image with: ", binary)
 
 
 @app.route('/', methods=['GET'])
@@ -22,8 +38,8 @@ def home():
     return "Service connected"
 
 
-@app.route('/upload', methods=['POST'])
-@cross_origin()
+@ app.route('/upload', methods=['POST'])
+@ cross_origin()
 def upload_image():
     image_id = request.args.get('id')
     print(request.data)
@@ -34,11 +50,16 @@ def upload_image():
     return "Image recieved"
 
 
-@app.route('/download', methods=['GET'])
+@ app.route('/download', methods=['GET'])
 def download_image():
     image_id = request.args.get('id')
     print(request.data)
     return send_file('uploads/' + image_id + '.png', mimetype='image/gif')
+
+
+@ app.route('/binaries', methods=['GET'])
+def api_all():
+    return jsonify(binaries)
 
 
 app.run()
